54eea253e1dd892571e2f179e58be53f65d7a66f
Terence O'Donoghue <terence@creativelittledots.co.uk>
2021-01-22 01:07:15 +0000

    Update wp-acf-rest-api-blocks.php

--- wp-acf-rest-api-blocks.php
+++ wp-acf-rest-api-blocks.php
@@ -5,7 +5,7 @@
 * Plugin Name: WP ACF Rest API Blocks
 * Version: 1.0.0
 * Plugin URI: https://github.com/wp-kit/wp-acf-rest-api-blocks
-* Description: The plugin sends Gutneberg Blocks into REST API responses under 'gblocks' property and detects if ACF is installed, if so it runs the acf_setup_meta + get_fields hack to output acf fields within the response for acf blocks
+* Description: The plugin sends Gutenberg Blocks into REST API responses under 'gblocks' property and detects if ACF is installed, if so it runs the acf_setup_meta + get_fields hack to output acf fields within the response for acf blocks
 * Author: WPKit
 */
 
